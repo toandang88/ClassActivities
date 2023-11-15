1e7e6cb108e094811b7eaa2c7ced57fc2ff9d9aa
Toan Dang <toandangqc@gmail.com>
2023-11-14 21:04:33 -0600

    New code added for Week2

---
+++ week2/2.2.1-JSVariablesAndPrimitives.js
@@ -0,0 +1,21 @@
+// create a constant variable (const)
+var name = "Toan Dang"
+// create a variable that can be reassigned (let)
+let language = "English"
+// create three variables and assign them values of different data types (=)
+let age = 35
+const whatIf = true
+
+// print the types of two variables that reference two different data types (typeof)
+// TIP: console.log() prints a value in the console/terminal
+console.log(typeof age);
+
+// change the value referenced by a variable (dynamic typing)
+
+// print the type of the variable you just changed (typeof)
+
+// create variables and give them values to complete the sentences that will print  below.
+// Which variables need to be created?
+// What type of data needs to go in each variable?
+
+// console.log("Hello, my name is " + {add a name} + ", and I am learning " + {add a language} + ". I have been practicing for only " +  {add a number} + " weeks, so my status as a master is: " + {true or false} + ".");
---
+++ week2/2.2.2-JSVariablesAndPrimitives.js
@@ -0,0 +1,31 @@
+// create a variable called "name" that references a string
+const name = "Toan Dang"
+// create a variable called "favoriteSong" that references a string
+const favoriteSong = "This is my song"
+// create a variable called "wage" that references a number
+let wage = 2000
+// create a variable called "age" that references a number
+let age = 35
+// create a variable called "onlyChild" that references a boolean
+let onlyChild = true
+// create a variable called "satisfied" that references a boolean
+
+// create a variable called "favoriteThing" that references ANY primitive value
+// print the data type of "favoriteThing"
+
+// You heard a new song that became your favorite.
+// Reassign the variable "favoriteSong"
+
+// You no longer like the type of data stored in "favoriteThing"
+// Reassign "favoriteThing" to a different primitive data type and print its type
+
+// Happy Birthday!
+// Reassign "age" to a new value
+
+// You got a big raise at work.
+// Reassign "wage" to a new value
+
+// Your raise changed your job satisfaction.
+// Assign "satisfied" to true
+
+// use "console.log()" and "+" to print a sentence that includes two of the above variables
